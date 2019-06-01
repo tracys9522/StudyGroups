b9d19359942bedcddb562fdc0f6845c5c0f9ca40
Aneesh <aneakell@cisco.com>
2019-06-01 11:11:47 -0700

    to merge

--- app/src/main/java/com/example/studygroups/PendingInvitationsActivity.java
+++ app/src/main/java/com/example/studygroups/PendingInvitationsActivity.java
@@ -28,6 +28,7 @@ public class PendingInvitationsActivity extends AppCompatActivity {
 
         listview.setAdapter(arrayAdapter);
 
+
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
