f62ad0742d48354b9519886e251a4371bc964768
gsun <gsun@scu.edu>
2019-06-02 10:44:11 -0700

    file retrieve...waiting for firestore and storage connection

--- app/src/main/java/com/example/studygroups/Messages.java
+++ app/src/main/java/com/example/studygroups/Messages.java
@@ -2,6 +2,7 @@ package com.example.studygroups;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -13,11 +14,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Messages extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    ListView retrieve;
+    ArrayList<String> currentGroups = new ArrayList<>();
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference collectionProfile = db.collection("User Profile");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +62,43 @@ public class Messages extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        retrieve = findViewById(R.id.retrieve);
+
+        Query searchUser = collectionProfile.whereEqualTo("username", PostLoginActivity.username);
+        searchUser.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        UserProfile user = document.toObject(UserProfile.class);
+                        currentGroups = user.groups;
+                    }
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(Messages.this, android.R.layout.simple_selectable_list_item, currentGroups);
+                    adapter.notifyDataSetChanged();
+                    retrieve.setAdapter(adapter);
+                }
+            }
+        });
+
+        retrieve.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String group_name = currentGroups.get(position);
+                Intent intent = new Intent(Messages.this, GroupFile.class);
+                intent.putExtra("group",group_name);
+                startActivity(intent);
+            }
+        });
+        /*
+        // ImageView in your Activity
+        ImageView imageView = findViewById(R.id.imageView);
+
+        GlideApp.with(this)
+                .load(storageReference)
+                .into(imageView);
+        */
+
     }
 
     @Override
